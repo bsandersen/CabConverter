4cc91ce15443c93abb811148263e0bea3fe460ea
andersen <bsandersen@mac.com>
2012-11-24 20:21:56 -0500

    Version file. Holds the version string for the program.

---
+++ Cab-converter/src/com/bsandersen/CabConverter/Version.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * CabConverter: A Cabrillo generation tool for MacLoggerDX
+ * Original concept and author: B. Scott Andersen (NE1RD)
+ */
+package com.bsandersen.CabConverter;
+
+/**
+ * Holds the version of the software.
+ * @author B. Scott Andersen
+ */
+public class Version {
+	public final static String version = "CabConverter 2.0a";
+}
