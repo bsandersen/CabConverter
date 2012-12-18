12ee6062658798c5fefbaf732fe908f24b76f012
andersen <bsandersen@mac.com>
2012-12-17 19:00:22 -0500

    Space needed after verbatim mode pass through for QSO. Fixed. Contesting receipe documentation update to reflect this passthrough. Revision history added to About document. README updated to include pointer to revision history. Version bumped to d.

--- Cab-converter/src/com/bsandersen/CabConverter/Version.java
+++ Cab-converter/src/com/bsandersen/CabConverter/Version.java
@@ -13,5 +13,5 @@ package com.bsandersen.CabConverter;
  * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
  */
 public class Version {
-	public final static String version = "CabConverter 2.0c";
+	public final static String version = "CabConverter 2.0d";
 }
