01178a385de73e7e87f8bbeffabd12a4701ac8b9
andersen <bsandersen@mac.com>
2012-12-29 17:44:11 -0500

    JavaDoc package description.

---
+++ Cab-converter/src/com/bsandersen/CabConverter/package-info.java
@@ -0,0 +1,29 @@
+/**
+<h2>Package Overview</h2>
+<p>
+This package (com.bsandersen.CabConverter) contains the logic for CabConverter, the
+amateur radio contest utility used to convert radio log files in the standard 
+<a href="http://www.adif.org/" alt="ADIF standard web site">.adif</a>
+format to a generalized format called 
+<a href="http://www.kkn.net/~trey/cabrillo/" alt="Cabrillo description web site">Cabrillo</a>.
+This program is the second generation of the utility made public. The original program
+(CabConverter 1.x) was distributed and supported within the Yahoo! Group 
+<a href="http://groups.yahoo.com/group/cab-converter/">Cab-converter</a>. (Membership is 
+required to gain access to files or messages.)
+</p>
+<p>
+There is significant documentation on program requirements, usage, and ways to extend
+the program through additional contest recipes within the "Documentation" folder.
+</p>
+
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US">
+<img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a>
+<br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">CabConverter 2</span> by 
+<a xmlns:cc="http://creativecommons.org/ns#" href="http://www.bsandersen.com" property="cc:attributionName" rel="cc:attributionURL">B. Scott Andersen</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.
+Permissions beyond the scope of this license may be available at <a xmlns:cc="http://creativecommons.org/ns#" href="http://www.bsandersen.com" rel="cc:morePermissions">http://www.bsandersen.com</a>.
+
+@since 2.0
+@author B. Scott Andersen (NE1RD)
+ */
+
+package com.bsandersen.CabConverter;
